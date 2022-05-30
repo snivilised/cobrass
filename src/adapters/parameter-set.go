40003ee37c0e8f9a2c021832430848babf67ea90
plastikfan <plastikfan@outlook.com>
2022-05-30 16:02:26 +0100

    feat(adapters): add parameter set (#6)

---
+++ src/adapters/parameter-set.go
@@ -0,0 +1,37 @@
+package adapters
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type GenericParameterSet map[string]any
+
+// NewParameterSet is a generic function on comparable type T which when
+// given a map containing field name to values, creates the native
+// object required by the client. It is intead that the user should use
+// Cobra's VisitAll facility on the command to create the values asscoiated
+// with each flag/argument. The comparable type T is the native parameter
+// set object that can be futher used by the cli without tight coupling to
+// Cobra.
+// Panics, if instantiated with anything other than a struct.
+//
+func NewParameterSet[T comparable](params GenericParameterSet) *T {
+	target := new(T)
+
+	refElemStruct := reflect.ValueOf(target).Elem()
+	refTypeOfStruct := refElemStruct.Type()
+
+	if reflect.TypeOf(*target).Kind() == reflect.Struct {
+		for i, n := 0, refElemStruct.NumField(); i < n; i++ {
+			name := refTypeOfStruct.Field(i).Name
+			value := params[name]
+			refElemStruct.Field(i).Set(reflect.ValueOf(value))
+		}
+	} else {
+		name := reflect.TypeOf(*target).Name()
+		panic(fmt.Sprintf("the native parameter set object ('%v') must be a structure", name))
+	}
+
+	return target
+}
---
+++ src/adapters/parameter-set_test.go
@@ -0,0 +1,124 @@
+package adapters
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type OutputFormatEnum int
+
+const (
+	XmlFormatEn OutputFormatEnum = iota + 1
+	JsonFormatEn
+	TextFormatEn
+	ScribbleFormatEn
+)
+
+type InfexionShapeEnum int
+
+const (
+	FullNameShapeEn InfexionShapeEnum = iota + 1
+	NameShapeEn
+	SubPathShapeEn
+)
+
+type TravseralStratgeyEnum int
+
+const (
+	TraverseAllEn TravseralStratgeyEnum = iota + 1
+	TraverseLeafEn
+)
+
+type ParameterSetSuite struct {
+	suite.Suite
+
+	Native   FooParameterSet
+	Expected GenericParameterSet
+}
+
+func (suite *ParameterSetSuite) SetupTest() {
+	suite.Native = FooParameterSet{
+		Directory:           "/once/upon/a/time",
+		Output:              "foo-bar",
+		Format:              XmlFormatEn,
+		Shape:               SubPathShapeEn,
+		IsConcise:           true,
+		Strategy:            TraverseLeafEn,
+		IsOverwrite:         false,
+		SegmentsFilePattern: "*infex*",
+	}
+
+	// This is the item that is generated via visiting the Cobra command's flags set
+	// We don't want to force the client to use a map (generic parameter set),
+	// instead we want to use a native abstraction, so turn it into a parameter set, ie:
+	// --> FooParameterSet
+	//
+	suite.Expected = GenericParameterSet{
+		"Directory":           "/once/upon/a/time",
+		"Output":              "foo-bar",
+		"Format":              XmlFormatEn,
+		"Shape":               SubPathShapeEn,
+		"IsConcise":           true,
+		"Strategy":            TraverseLeafEn,
+		"IsOverwrite":         false,
+		"SegmentsFilePattern": "*infex*",
+	}
+}
+
+func TestParameterSetSuite(t *testing.T) {
+	suite.Run(t, new(ParameterSetSuite))
+}
+
+type FooParameterSet struct {
+	Directory           string
+	Output              string
+	Format              OutputFormatEnum
+	Shape               InfexionShapeEnum
+	IsConcise           bool
+	Strategy            TravseralStratgeyEnum
+	IsOverwrite         bool
+	SegmentsFilePattern string
+}
+
+// possibly this should be called ParameterSetFromMap
+//
+func NewFooParameterSet(params GenericParameterSet) *FooParameterSet {
+	return NewParameterSet[FooParameterSet](params)
+}
+
+func (suite *ParameterSetSuite) TestNativeParameterCreationFromGenericSetWithReflection() {
+	//
+	// This test case is an excercise in using reflection and thus would not
+	// normally be tested this way. See the other test TestNativeParameterCreationFromGenericSet.
+	//
+	// based on the laws of reflection (https://go.dev/blog/laws-of-reflection)
+
+	target := NewFooParameterSet(suite.Expected)
+
+	refElemStruct := reflect.ValueOf(target).Elem()
+	refTypeOfStruct := refElemStruct.Type()
+
+	if reflect.TypeOf(*target).Kind() == reflect.Struct {
+		for i, n := 0, refElemStruct.NumField(); i < n; i++ {
+			name := refTypeOfStruct.Field(i).Name
+			value := suite.Expected[name]
+			assert.Equal(suite.T(), suite.Expected[name], value)
+		}
+	}
+}
+
+func (suite *ParameterSetSuite) TestNativeParameterCreationFromGenericSet() {
+	actual := NewFooParameterSet(suite.Expected)
+
+	assert.Equal(suite.T(), suite.Expected["Directory"], actual.Directory)
+	assert.Equal(suite.T(), suite.Expected["Output"], actual.Output)
+	assert.Equal(suite.T(), suite.Expected["Format"], actual.Format)
+	assert.Equal(suite.T(), suite.Expected["Shape"], actual.Shape)
+	assert.Equal(suite.T(), suite.Expected["IsConcise"], actual.IsConcise)
+	assert.Equal(suite.T(), suite.Expected["Strategy"], actual.Strategy)
+	assert.Equal(suite.T(), suite.Expected["IsOverwrite"], actual.IsOverwrite)
+	assert.Equal(suite.T(), suite.Expected["SegmentsFilePattern"], actual.SegmentsFilePattern)
+}
